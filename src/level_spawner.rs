342a871c590ccea702918f710ef31d3c443bb338
Cedric Martens <cedricmartens98@gmail.com>
2025-10-05 23:15:56 -0400

    Level 2

--- src/level_spawner.rs
+++ src/level_spawner.rs
@@ -513,7 +513,7 @@ fn load_level(
                             .insert(
                                 // should spawn at the tile position
                                 Transform::from_xyz(effective_x, 0.0, effective_z)
-                                    .with_scale(Vec3::splat(0.5 * TILE_SIZE)),
+                                    .with_scale(Vec3::splat(0.25 * TILE_SIZE)),
                             )
                             .insert(LevelElement);
                     }),
