d782fe2982deac386e8a54d665f2a738ab98f3e1
Alexander Winter <a.w1nter@hotmail.com>
2025-10-06 14:34:44 -0400

    Mega level regactor

--- src/level_spawner.rs
+++ src/level_spawner.rs
@@ -359,7 +359,7 @@ fn load_level(
     let mars_texture = asset_server.load("mars.png");
     commands.spawn((
         LevelElement,
-        Mesh3d(meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(10.0)))),
+        Mesh3d(meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(20.0)))),
         MeshMaterial3d(materials.add(StandardMaterial {
             //base_color: Color::srgb(0.8, 0.35, 0.2), // Mars-colored (reddish-orange)
             base_color_texture: Some(mars_texture),
