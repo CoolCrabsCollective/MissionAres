eb9611622af7847ef6fc80cc7a4335d9c5870cbe
Alexander Winter <a.w1nter@hotmail.com>
2025-10-06 09:54:28 -0400

    Win timer and sound

--- src/level_spawner.rs
+++ src/level_spawner.rs
@@ -861,11 +861,13 @@ fn asset_loaded(
 }
 
 fn handle_puzzle_solved_event(
+    mut commands: Commands,
     mut events: EventReader<PuzzleResponseEvent>,
     mut level_spawn_request_writer: EventWriter<LevelSpawnRequestEvent>,
     levels: Res<Assets<GRADVM>>,
     level_handles: Res<GRADVM_ONVSTVS>,
-    active_level: Res<ActiveLevel>,
+    mut active_level: ResMut<ActiveLevel>,
+    asset_server: Res<AssetServer>,
 ) {
     for event in events.read() {
         if *event == PuzzleResponseEvent::Solved {
@@ -874,22 +876,28 @@ fn handle_puzzle_solved_event(
                 return;
             };
 
-            let Some(active_level) = levels.get(active_level_handle) else {
+            let Some(current_level) = levels.get(active_level_handle) else {
                 log::error!("No active level.");
                 return;
             };
 
             let Some(next_level_handle) = level_handles
                 .GRADVS
-                .get(active_level.INDEX as usize + 1)
+                .get(current_level.INDEX as usize + 1)
                 .or(level_handles.GRADVS.get(0))
             else {
                 log::error!("No next level.");
                 return;
             };
 
-            level_spawn_request_writer.write(LevelSpawnRequestEvent {
-                level: next_level_handle.clone(),
+            active_level.0 = Some(next_level_handle.clone());
+
+            commands.spawn((
+                AudioPlayer::new(asset_server.load("sfx/win.ogg")),
+                PlaybackSettings::DESPAWN,
+            ));
+            commands.spawn(ResetTimer {
+                timer: Timer::from_seconds(1.0, TimerMode::Once),
             });
         }
     }
